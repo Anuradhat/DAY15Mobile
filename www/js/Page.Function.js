8b905de2e81d47b00ebb24fca7d7bfa95796a9aa
anuradhat <anuradhath@live.com>
2015-12-07 10:56:50 +0530

    New Page Added

---
+++ www/js/Page.Function.js
@@ -0,0 +1,28 @@
+﻿function LoadModule() {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/DAY15WEB/OwnerTask.asmx/ModuleSelectAll",
+        data: "{'':''}",
+        contentType: "application/json; charset=utf-8",
+        success: ajaxCallSucceed,
+        dataType: "json",
+        failure: ajaxCallFailed
+    });
+
+    function ajaxCallSucceed(response) {
+        alert("Succeed Load Module...");
+        //$('#divLoading').hide();
+        //var workList = eval('(' + response.d + ')');
+        //pareseResult(workList);
+    }
+
+    function ajaxCallFailed(error) {
+        //$('#divLoading').hide();
+        alert('error: ' + error);
+        //$('#divResults').hide();
+    }
+
+    function pareseResult(workList) {
+        
+    }
+}
